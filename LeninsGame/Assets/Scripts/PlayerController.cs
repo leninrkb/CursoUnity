a36ee010341da539c9ea5fce5ea361b8b0336324
Lenin Acosta <rkb.lenin@gmail.com>
2023-06-07 19:14:25 -0500

    player controller

---
+++ LeninsGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private RigidBody2D rb;
+    public float velocidad_de_movimiento = 10;
+
+    private void Awake(){
+        rb = GetComponent<RigidBody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movimiento();
+    }
+
+
+    private void movimiento(){
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector2 direccion = new Vector2(x,y);
+        caminar(direccion);
+    }
+
+    private void caminar(Vector2 direccion){
+        rb.velocity = new Vector2(direccion.x * velocidad_de_movimiento, rb.velocity.y);
+    }
+}
