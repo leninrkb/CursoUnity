a6e2c336a045bacab58d203a9dfaf11ed287d785
Lenin Acosta <rkb.lenin@gmail.com>
2023-07-02 16:27:47 -0500

    genero matriz en tailmap

--- GenerarMapas/Assets/Scripts/Generador.cs
+++ GenerarMapas/Assets/Scripts/Generador.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Generador : MonoBehaviour
 {
-    public bool ejemplo;
+    public Tilemap canvas;
+    public TileBase tile;
+    public int filas;
+    public int columnas;
     
     public void generar_mapa(){
+        int[,] matriz = Metodos.generarMatriz(filas, columnas, true);
+        Metodos.generarMapa(matriz, canvas, tile);
         Debug.Log("generando mapa!");
     }
 
     public void limpiar_mapa(){
+        canvas.ClearAllTiles();
         Debug.Log("limpiando mapa!");
     }
 }
--- GenerarMapas/Assets/Scripts/Metodos.cs
+++ GenerarMapas/Assets/Scripts/Metodos.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Metodos : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+    public static int[,] generarMatriz(int ancho, int alto, bool llenar){
+        int[,] mapa = new int[ancho,alto];
+        for (int x = 0; x < ancho; x++)
+        {
+            for (int y = 0; y < alto; y++)
+            {
+                mapa[x,y] = llenar ? 1 : 0;
+            }
+        }
+        return mapa;
+    } 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+    public static void generarMapa(int[,] matriz, Tilemap canvas, TileBase tile){
+        canvas.ClearAllTiles();
+        // matriz.GetLength(0) obtengo filas
+        // matriz.GetLength(1) obtengo columnas
+        for (int row = 0; row < matriz.GetLength(0); row++)
+        {
+            for (int col = 0; col < matriz.GetLength(1); col++)
+            {
+                if(matriz[row,col] == 1){
+                    canvas.SetTile(new Vector3Int(row,col,0), tile);
+                }
+            }
+        }
+    }  
 }
